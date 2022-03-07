f038e8315f0385542e705843d8932c4337760ae1
Ambikesh Prajapati <98260044+ambikeshprajapati@users.noreply.github.com>
2022-03-08 00:42:33 +0530

    Add files via upload
    This is a Rock Paper Scissor game that is written in python language.

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,23 +1,31 @@
 import random
+def file(count):
+    with open("Hiscore.txt","r") as f:
+        hiscore=f.read()
+        if int(hiscore)<count:
+            with open("Hiscore.txt","w") as f:
+                f.write(str(count))
 def game(comp,you):
-    if   comp=="R" and you=="P":
+    if   comp=="R" and (you=="P" or you=="p"):
         return True
-    elif comp=="R" and you=="S":
+    elif comp=="R" and (you=="S" or you=="s"):
         return False
-    elif comp=="R" and you=="R":
+    elif comp=="R" and (you=="R" or you=="r"):
         return None
-    elif comp=="P" and you=="S":
+    elif comp=="P" and (you=="S" or you =="s"):
         return True
-    elif comp=="P" and you=="R":
+    elif comp=="P" and (you=="R" or you =="r"):
         return False
-    elif comp=="P" and you=="P":
+    elif comp=="P" and (you=="P" or you =="p"):
         return None
-    elif comp=="S" and you=="R":
+    elif comp=="S" and (you=="R" or you =="r"):
         return True
-    elif comp=="S" and you=="P":
+    elif comp=="S" and (you=="P" or you =="p"):
         return False
-    elif comp=="S" and you=="S":
+    elif comp=="S" and (you=="S" or you =="s"):
         return None
+    else:
+        return 2
 t=int(input("Enter the no. of times you want to play \n"))
 count =0
 T=t
@@ -40,5 +48,9 @@ while t!=0:
         count += 2
     elif game(comp,you)==False:
         print("You lose :( ")
+    else:
+        print("Please enter a valid character")
+        t +=1
     t -= 1
 print("Your score is ", count,"/",T*2)
+file(count) #to store high score in the Hiscore.txt file
